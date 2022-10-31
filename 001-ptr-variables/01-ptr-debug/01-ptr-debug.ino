d7338542a8d47ebf3006b381c8512370a7be5034
Michael Cheich <mcheich@gmail.com>
2022-10-31 15:01:54 -0400

    pointer 1 shell

---
+++ 001-ptr-variables/01-ptr-debug/01-ptr-debug.ino
@@ -0,0 +1,26 @@
+
+/*
+  1.  This one is for rote memorization.  
+      Pretend like you are Bart Simpson in detention and write the sentence,
+      or say it out as many times as necessary until it spills out of your brain with no effort:
+
+      “A pointer is a variable that holds the memory address of another variable.”
+
+  2.  Make a list of common data types and the corresponding number of bytes they take up in memory on an Arduino UNO.  
+      For example,  integer - 2 bytes, char 1 byte
+
+  3.  Open a new Arduino sketch and declare pointers of each of the types you have in your table above.  
+      Remember, when declaring a pointer variable, the * asterisk (aka dereference / indirection operator) 
+      is used before the name to let the compiler know this will be a pointer variable.
+
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
